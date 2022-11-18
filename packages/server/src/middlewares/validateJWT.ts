0590b494804866bfde7f89d03fa97507bbc4a3b9
riquelme <riquelmebandeira1@outlook.com>
2022-11-18 15:44:50 -0300

    feat: add token validation middleware

---
+++ packages/server/src/middlewares/validateJWT.ts
@@ -0,0 +1,35 @@
+import { NextFunction, Request, Response } from 'express'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import * as dotenv from 'dotenv'
+import prisma from '../database/prismaClient'
+dotenv.config()
+
+const validateJWT = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<Response | void> => {
+  const token = req.headers.authorization
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token not found.' })
+  }
+
+  try {
+    const secret = process.env.JWT_SECRET as string
+
+    const { username } = jwt.verify(token, secret) as JwtPayload
+
+    const user = await prisma.user.findUnique({ where: { username } })
+
+    if (!user) return res.status(404).json({ message: 'User not found' })
+
+    req.body = user
+
+    next()
+  } catch (err) {
+    return res.status(401).json({ message: 'The token is invalid.' })
+  }
+}
+
+export default validateJWT
