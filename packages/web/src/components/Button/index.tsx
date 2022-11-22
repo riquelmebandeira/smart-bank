8b7d927a8c544e1c915924b8aa9041d18c5fe8bc
riquelme <riquelmebandeira1@outlook.com>
2022-11-21 21:47:17 -0300

    feat: add onClick prop to button component

--- packages/web/src/components/Button/index.tsx
+++ packages/web/src/components/Button/index.tsx
@@ -4,13 +4,19 @@ type ButtonProps = {
   text: string
   className?: string
   variant: 'primary' | 'ghost'
+  onClick?: () => void
 }
 
 const Button = (props: ButtonProps): JSX.Element => {
-  const { className, variant = 'primary', text } = props
+  const { className, variant = 'primary', text, onClick } = props
 
   return (
-    <button className={`button button__${variant} ${className}`}>{text}</button>
+    <button
+      className={`button button__${variant} ${className}`}
+      onClick={onClick}
+    >
+      {text}
+    </button>
   )
 }
 
