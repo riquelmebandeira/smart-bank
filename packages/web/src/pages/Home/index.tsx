15edbc02a2ea3ad88dc2778c68b599582cb49e57
riquelme <riquelmebandeira1@outlook.com>
2022-11-23 00:57:45 -0300

    fix: add media queries to fix minor breaks

--- packages/web/src/pages/Home/index.tsx
+++ packages/web/src/pages/Home/index.tsx
@@ -1,5 +1,20 @@
+import TransferForm from '../../components/TransferForm'
+import Transactions from '../../components/Transactions'
+import Header from '../../components/Header'
+import './styles.scss'
+
 const Home = (): JSX.Element => {
-  return <h1>Olá, usuário</h1>
+  return (
+    <>
+      <Header />
+      <main className="home">
+        <section className="transfer">
+          <TransferForm />
+        </section>
+        <Transactions />
+      </main>
+    </>
+  )
 }
 
 export default Home
