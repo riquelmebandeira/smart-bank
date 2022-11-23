79f1f2d399909616728eb2feaa7871526f646f9b
riquelme <riquelmebandeira1@outlook.com>
2022-11-22 23:06:29 -0300

    feat: add loading component

---
+++ packages/web/src/components/Loading/index.tsx
@@ -0,0 +1,14 @@
+import './styles.scss'
+
+const Loading = (): JSX.Element => {
+  return (
+    <div className="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Loading
