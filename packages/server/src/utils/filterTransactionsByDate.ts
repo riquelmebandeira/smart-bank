79aaf8dc38321c5813effeef81ad12eb7d2e6de5
riquelme <riquelmebandeira1@outlook.com>
2022-11-22 21:39:16 -0300

    fix: filter transactions method

--- packages/server/src/utils/filterTransactionsByDate.ts
+++ packages/server/src/utils/filterTransactionsByDate.ts
@@ -4,7 +4,11 @@ const filterTransactionsByDate = (
   date: string,
   transactions: TransactionsWithDebitedUser[]
 ) => {
-  return transactions.filter(t => t.createdAt.toLocaleDateString() === date)
+  return transactions.filter(
+    t =>
+      t.createdAt.toLocaleDateString() ===
+      new Date(date.replace(/-/g, '/')).toLocaleDateString()
+  )
 }
 
 export { filterTransactionsByDate }
