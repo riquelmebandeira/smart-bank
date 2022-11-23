0a6ea3aa4a96af94e5cc2b275197b129415be8df
riquelme <riquelmebandeira1@outlook.com>
2022-11-22 21:38:43 -0300

    fix: query params date validation

--- packages/server/src/middlewares/validateQueryParams.ts
+++ packages/server/src/middlewares/validateQueryParams.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 import Joi from 'joi'
 
-const DATE_REGEX = new RegExp(/^[0-9]{2}[/]{1}[0-9]{2}[/]{1}[0-9]{4}$/)
+const DATE_REGEX = new RegExp(/^[0-9]{4}[-]{1}[0-9]{2}[-]{1}[0-9]{2}$/)
 
 const queryParamsSchema = Joi.object({
   category: Joi.string().valid('debited', 'credited'),
